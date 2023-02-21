7e7193bcbbe8348ab619fbfc8364d62588f1624e
harel <harel_shalom@yahoo.com>
2023-02-21 22:14:24 +0200

    create header & footer components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,18 @@
 // import "./App.css";
+import { Container } from "react-bootstrap";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
 
 function App() {
   return (
     <div className="App">
-      <h1>E-commerce</h1>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <h1>E-commerce</h1>
+        </Container>
+      </main>
+      <Footer />
     </div>
   );
 }
